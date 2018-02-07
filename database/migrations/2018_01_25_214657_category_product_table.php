e970db5da7581a1c541b3971fdb6bbbb45f32f65
Hassan Abas <hassapasha@yahoo.com>
2018-02-07 08:26:34 +0200

    -

--- database/migrations/2018_01_25_214657_category_product_table.php
+++ database/migrations/2018_01_25_214657_category_product_table.php
@@ -12,18 +12,18 @@ class CategoryProductTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {/*
         Schema::create('category_product', function (Blueprint $table) {
             $table->integer('category_id')->unsigned();
             $table->integer('product_id')->unsigned();
             $table->foreign('category_id')->references('id')->on('categories');
             $table->foreign('product_id')->references('id')->on('products');
-        });
+        });*/
     }
 
 
     public function down()
     {
-        Schema::dropIfExists('category_product');
+       // Schema::dropIfExists('category_product');
     }
 }
