a9a0eebe21b55ac1ae5bef5b79af980ff7305067
Hassan Abas <hassapasha@yahoo.com>
2018-02-07 07:37:02 +0200

    -

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        //DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        /*
         User::truncate();
         Category::truncate();
         Product::truncate();
         Transaction::truncate();
-
+*/
         User::flushEventListeners();
         Category::flushEventListeners();
         Product::flushEventListeners();
@@ -29,12 +30,12 @@ class DatabaseSeeder extends Seeder
         $productsQty    = 1000;
         $transactionQty = 1000;
         factory(User::class,$userQty)->create();
-
+        /*
         factory(Category::class,$categoriesQty)->create();
         factory(Product::class,$productsQty)->create()->each(function ($products){
             $categories = Category::all()->random(mt_rand(1,5))->pluck('id');
             $products->categories()->attach($categories);
         });
-        factory(Transaction::class,$transactionQty)->create();
+        factory(Transaction::class,$transactionQty)->create();*/
     }
 }
